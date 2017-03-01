7eb41172965ef84f55225a4fc4176570f0929e1d
SMUSFTC <aamir.djearam@smus.ca>
2017-03-01 00:39:00 -0800

    Animation system and dragging works. // TODO: Make animation work on each card.

--- DragableCardsUI/MainWindow.xaml.cs
+++ DragableCardsUI/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +24,15 @@ namespace DragableCardsUI
         public MainWindow()
         {
             InitializeComponent();
+            Storyboard cardDragStart = Resources["CardDragStartAnimation"] as Storyboard;
+            Storyboard cardDragStop = Resources["CardDragStopAnimation"] as Storyboard;
+            DragablzItem.IsDraggingChanged += (s, e) =>
+            {
+                if (DragablzItem.IsDragging) cardDragStart.Begin();
+                else cardDragStop.Begin();
+            };
+            DragHandle.MouseEnter += (s, e) => Cursor = Cursors.SizeAll;
+            DragHandle.MouseLeave += (s, e) => Cursor = Cursors.Arrow;
         }
     }
 }
